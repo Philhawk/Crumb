a5f45881db705fdd0bf0b38dd7a85eebecd9d9b5
Philip Jacob <pkjacob@gmail.com>
2014-10-08 16:58:05 +1100

    removed unstaged migrations

--- db/migrate/20141003083306_create_mealplans_recipes.rb
+++
@@ -1,8 +0,0 @@
-class CreateMealplansRecipes < ActiveRecord::Migration
-  def change
-    create_table :mealplans_recipes, :id => false do |t|
-    	t.integer :mealplan_id
-    	t.integer :recipe_id
-    end
-  end
-end
